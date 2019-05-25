5c33115d7895c69ca4bc082e5b1590e9a4841c77
Guilherm Santos <gui07015@gmail.com>
2019-05-25 14:45:51 -0300

    Import component  Header,  import style content and import Routes. Add BrowserRoutes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import Header from './components/Header';
 
-import { Wrapper, Container } from './styles/components';
+import { Wrapper, Container, Content } from './styles/components';
+
+import Routes from './routes';
 
 const App = () => (
   <>
-    <GlobalStyle />
-    <Wrapper>
-      <Container>
-        <Sidebar />
-      </Container>
-      <Player />
-    </Wrapper>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Wrapper>
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
   </>
 );
 
